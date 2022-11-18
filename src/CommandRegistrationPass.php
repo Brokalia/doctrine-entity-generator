137af9a7988e55586ca16d39f0fbd77713a787a4
chuano <chuano@gmail.com>
2022-11-18 23:11:37 +0100

    Register command

---
+++ src/CommandRegistrationPass.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brokalia\DoctrineEntityGenerator;
+
+use Brokalia\DoctrineEntityGenerator\Command\DoctrineEntityGeneratorCommand;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CommandRegistrationPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $container->register('doctrine_entity_generator.command', DoctrineEntityGeneratorCommand::class)
+            ->addTag('console.command');
+    }
+}
--- src/DoctrineEntityGeneratorBundle.php
+++ src/DoctrineEntityGeneratorBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Brokalia\DoctrineEntityGenerator;
 
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class DoctrineEntityGeneratorBundle extends AbstractBundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new CommandRegistrationPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 10);
+    }
 }
